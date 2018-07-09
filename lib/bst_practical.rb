96bf6be678286b9bae8be49ae22bf69229c9c4e5
Shawn Salat <shawn.salat@gmail.com>
2018-07-09 11:05:39 -0700

    finish binary search tree project

--- lib/bst_practical.rb
+++ lib/bst_practical.rb
@@ -1,3 +1,8 @@
+require_relative 'binary_search_tree'
 
 def kth_largest(tree_node, k)
+  tree = BinarySearchTree.new
+  tree.root = tree_node
+  arr = tree.in_order_traversal
+  tree.find(arr[-k])
 end
